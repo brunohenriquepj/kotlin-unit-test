3f5610f8e0c6289475a3fe4023dfcc033e75c008
Bruno Pereira <bruno.henriquepj@gmail.com>
2021-05-14 02:50:10 -0300

    Add tests

--- src/test/kotlin/TestWithMockk1.kt
+++ src/test/kotlin/TestWithMockk1.kt
@@ -22,4 +22,27 @@ class TestWithMockk1 {
             doc2.value2
         }
     }
+
+    @Test
+    fun `SystemUnderTest set values`() {
+        val depedency1 = Depedency1(1)
+        val depedency2 = Depedency2("2")
+
+        val sut = SystemUnderTest(depedency1, depedency2)
+
+        assertEquals(depedency1, sut.depedency1)
+        assertEquals(depedency2, sut.depedency2)
+    }
+
+    @Test
+    fun `dependency 1 set values`() {
+        val depedency1 = Depedency1(1)
+        assertEquals(1, depedency1.value1)
+    }
+
+    @Test
+    fun `dependency 2 set values`() {
+        val depedency2 = Depedency2("2")
+        assertEquals("2", depedency2.value2)
+    }
 }
