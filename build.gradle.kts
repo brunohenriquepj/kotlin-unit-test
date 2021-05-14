595232a05e694781fcc445738d777532176b666d
Bruno Pereira <bruno.henriquepj@gmail.com>
2021-05-14 03:04:37 -0300

    Add github actions (#1)
    * Create blank.yml
    
    * Rename github action
    
    * Add github action steps
    
    * Testing github action with fail test
    
    * Exclude test task from gradle build
    
    * Add sonar scan
    
    * Change test to fail
    
    * add readme
    
    * Add sonar project properties
    
    * Run sonar always
    
    * enable gradle daemon
    
    * Add sonar project properties
    
    * Update readme
    
    * Remove sonar source property
    
    * Fix test
    
    * Move classes to src
    
    * Add jacoco code coverage plugin
    
    * Add codecov github action
    
    * Fix actions
    
    * Add tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.5.0"
     application
+    jacoco
 }
 
 group = "me.bruno"
@@ -19,6 +20,30 @@ dependencies {
 
 tasks.test {
     useJUnit()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+}
+
+jacoco {
+    toolVersion = "0.8.6"
+    //reportsDirectory.set(layout.buildDirectory.dir("customJacocoReportDir"))
+}
+
+tasks.jacocoTestReport {
+    reports {
+        csv.isEnabled = false
+        xml.isEnabled = true
+        html.isEnabled = true
+        //html.destination = layout.buildDirectory.dir("jacocoHtml").get().asFile
+    }
+}
+
+tasks.register<JacocoReport>("applicationCodeCoverageReport") {
+    executionData(tasks.run.get())
+    sourceSets(sourceSets.main.get())
 }
 
 tasks.withType<KotlinCompile>() {
@@ -26,5 +51,5 @@ tasks.withType<KotlinCompile>() {
 }
 
 application {
-    mainClassName = "MainKt"
+    mainClass.set("MainKt")
 }
--- src/test/kotlin/TestWithMockk1.kt
+++ src/test/kotlin/TestWithMockk1.kt
@@ -4,15 +4,6 @@ import io.mockk.verify
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
 
-class Depedency1(val value1: Int)
-class Depedency2(val value2: String)
-
-class SystemUnderTest(val depedency1: Depedency1, val depedency2: Depedency2) {
-    fun calculate(): Int {
-        return depedency1.value1 + depedency2.value2.toInt()
-    }
-}
-
 class TestWithMockk1 {
     @Test
     fun `calculate adds values`() {
@@ -30,5 +21,28 @@ class TestWithMockk1 {
             doc1.value1
             doc2.value2
         }
+    }
+
+    @Test
+    fun `SystemUnderTest set values`() {
+        val depedency1 = Depedency1(1)
+        val depedency2 = Depedency2("2")
+
+        val sut = SystemUnderTest(depedency1, depedency2)
+
+        assertEquals(depedency1, sut.depedency1)
+        assertEquals(depedency2, sut.depedency2)
+    }
+
+    @Test
+    fun `dependency 1 set values`() {
+        val depedency1 = Depedency1(1)
+        assertEquals(1, depedency1.value1)
+    }
+
+    @Test
+    fun `dependency 2 set values`() {
+        val depedency2 = Depedency2("2")
+        assertEquals("2", depedency2.value2)
     }
 }
