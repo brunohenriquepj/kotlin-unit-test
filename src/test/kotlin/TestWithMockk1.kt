15abce8231de3f036cc74353de941c7c0d7f5189
Bruno Pereira <bruno.henriquepj@gmail.com>
2021-05-15 23:10:51 -0300

    Exclude test folder on sonar coverage (#4)
    * Exclude test folder on sonar coverage
    
    * Refactor tests

--- src/test/kotlin/TestWithMockk1.kt
+++ src/test/kotlin/TestWithMockk1.kt
@@ -13,7 +13,7 @@ class TestWithMockk1 {
         every { doc1.value1 } returns 5
         every { doc2.value2 } returns "6"
 
-        val actual = SystemUnderTest(doc1, doc2).calculate()
+        var actual = SystemUnderTest(doc1, doc2).calculate()
 
         assertEquals(11, actual)
 
@@ -21,28 +21,5 @@ class TestWithMockk1 {
             doc1.value1
             doc2.value2
         }
-    }
-
-    @Test
-    fun `SystemUnderTest set values`() {
-        val depedency1 = Depedency1(1)
-        val depedency2 = Depedency2("2")
-
-        val sut = SystemUnderTest(depedency1, depedency2)
-
-        assertEquals(depedency1, sut.depedency1)
-        assertEquals(depedency2, sut.depedency2)
-    }
-
-    @Test
-    fun `dependency 1 set values`() {
-        val depedency1 = Depedency1(1)
-        assertEquals(1, depedency1.value1)
-    }
-
-    @Test
-    fun `dependency 2 set values`() {
-        val depedency2 = Depedency2("2")
-        assertEquals("2", depedency2.value2)
     }
 }
